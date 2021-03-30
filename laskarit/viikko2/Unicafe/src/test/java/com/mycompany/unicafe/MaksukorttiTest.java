a53009c4b9ffa9912cfdbe500b7783f1b1ac913e
yahiamoh <eerifaj@gmail.com>
2021-03-30 22:32:34 +0300

    Testitiedostot muutettu

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,39 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoOikein() {
+        
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoa() {
+        kortti.lataaRahaa(100);
+        
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeKunRahaaTarpeeksi() {
+        kortti.lataaRahaa(990);
+        
+        kortti.otaRahaa(500);
+                
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuKunEiOleSaldoa() {
+        kortti.otaRahaa(100);
+        
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void metodiPalauttaaTrueJosRiittavastiSaldoa() {
+        
+        assertEquals(true, kortti.otaRahaa(10));
+    }
 }
